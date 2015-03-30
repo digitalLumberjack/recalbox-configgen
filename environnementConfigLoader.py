33024cfc6072132950d7892353ba0d3e52cedd59
skitoo <alexis@skitoo.net>
2015-03-30 12:35:14 +0200

    pep8 refacto

--- environnementConfigLoader.py
+++ environnementConfigLoader.py
@@ -4,14 +4,19 @@ import settings.emulationstationSettings as esSettings
 import settings.recalboxSettings as recalSettings
 import settings.libretroSettings as libretroSettings
 
-def loadEnvConfig(system) :
+
+def loadEnvConfig(system):
         smooth = esSettings.load("Smooth")
         shaders = esSettings.load("Shaders")
         ratio = esSettings.load("GameRatio")
-        video_mode = recalSettings.load(system+"_video_mode")
-        custom_emulator = recalSettings.load(system+"_emulator")
-        if shaders == "true" :
-                smooth = false
-        return { 'smooth' : smooth, 'shaders' : shaders, 'ratio' : ratio, 'video_mode' : video_mode,\
-                 'custom_emulator' : custom_emulator }
-
+        video_mode = recalSettings.load(system + "_video_mode")
+        custom_emulator = recalSettings.load(system + "_emulator")
+        if shaders == "true":
+            smooth = False
+        return {
+            'smooth': smooth,
+            'shaders': shaders,
+            'ratio': ratio,
+            'video_mode': video_mode,
+            'custom_emulator': custom_emulator
+        }
--- generators/libretro/libretroGenerator.py
+++ generators/libretro/libretroGenerator.py
@@ -4,13 +4,14 @@ settingsRoot = "/recalbox/configs/retroarch"
 settingsFileOrigin = settingsRoot + "/retroarchcustom.cfg.origin"
 settingsFile = settingsRoot + "/retroarchcustom.cfg"
 
+
 # Main entry of the module
 # Configure retroarch and return a command
-def generate(system, playersControllers) :
+def generate(system, playersControllers):
     # Write controllers configuration files
     # write options in retroarchcustom from env
     # set videomode
     # launch the emulator
     # video mode default
     print 'ok'
-    #return (video_mode, launch command)
+    # return (video_mode, launch command)
