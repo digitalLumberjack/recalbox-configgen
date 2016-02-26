f66204329a4284df7420f29f2b1d1c9b853b276a
ShunLe <flopigna@gmail.com>
2016-02-26 23:38:08 +0100

    Update emulatorlauncher.py

--- configgen/emulatorlauncher.py
+++ configgen/emulatorlauncher.py
@@ -71,6 +71,7 @@ emulators["atari7800"] = Emulator(name='atari7800', emulator='libretro', core='p
 emulators["prboom"] = Emulator(name='prboom', emulator='libretro', core='prboom')
 emulators["psx"] = Emulator(name='psx', emulator='libretro', core='pcsx_rearmed')
 emulators["cavestory"] = Emulator(name='cavestory', emulator='libretro', core='nxengine')
+emulators["dinothawr"] = Emulator(name='dinothawr', emulator='libretro', core='dinothawr')
 emulators["imageviewer"] = Emulator(name='imageviewer', emulator='libretro', core='imageviewer')
 emulators["scummvm"] = Emulator(name='scummvm', emulator='scummvm', videomode='default')
 
